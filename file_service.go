cff9bf04d61429f35c29af976f27a4ef4bc09faf
fadli <fadlirahmanfauzan04@gmail.com>
2023-01-14 11:22:46 +0700

    [feature] new feature AppendText and modified file name

--- file_service.go
+++ file_service.go
@@ -11,7 +11,7 @@ func ReadByLine(fileName string) {
 	if err != nil {
 		fmt.Println("Tidak dapat Membaca File")
 	}
-	
+
 	defer file.Close()
 
 	fileScan := bufio.NewScanner(file)
@@ -21,4 +21,18 @@ func ReadByLine(fileName string) {
 		fmt.Println(fileScan.Text())
 	}
 
+}
+
+func AppendText(fileName string,input string) {
+	file, err := os.OpenFile(fileName,os.O_APPEND | os.O_CREATE | os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	resultOfText := fmt.Sprintf("\n%s",input)
+
+	if _,err := file.WriteString(resultOfText); err != nil {
+		fmt.Println(err)
+	}
 }
